ddb513cdeafce115302bcccc86b4a1e7d7151587
LouiCUNI <louicuni@gmail.com>
2021-02-24 00:55:06 +0100

    little refacto and improvement of complexity

--- src/falcon_millenium_computer_back.py
+++ src/falcon_millenium_computer_back.py
@@ -1,8 +1,83 @@
 import json
+import sqlite3 as sql
+from universe_requetor import *
+from utils import *
+from pathlib import Path
+from falcon_millenium_computer_back import *
 
 
-def read_file(path):
-    with open(path) as json_file:
-        data = json.load(json_file)
-    return data
+class falco_M_computer:
 
+    def __init__(self, vaisseau_data, empire_data ):
+        self.vaisseau_data = vaisseau_data
+        self.empire_data = empire_data
+        db_path = Path.cwd()
+        db_path = db_path.parent / 'database' 
+        self.requetor = universe_requetor(db_path / vaisseau_data['routes_db'])
+        self.dico = {}
+
+    def build_dico(self):
+    #build dico that represent all possible actions in the universe (the database)
+        self.dico = {'Tatooine':{},
+                'Dagobah' : {},
+                'Hoth': {},
+                'Endor': {}}#a modifier
+        self.dico[self.vaisseau_data['departure']][0] = [{"fuel": 6, "danger": 0}]
+
+        for day in range(self.empire_data['countdown'] + 1):
+            for planet in self.dico.keys():
+                if planet != 'Endor':#pas top, pas besoin d'update sur la derniere planete
+                    actions = self.requetor.get_possible_action(planet)
+                    if day in self.dico[planet].keys():#virrable
+                        possibilities = build_pareto(self.dico[planet][day])
+                        self.dico[planet][day] = possibilities
+                        for p in possibilities:
+                            self.update_dico( actions, p, [day, planet] )
+                else:
+                    if day in self.dico[planet].keys():#virrable
+                        possibility = safest(self.dico[planet][day])
+                        self.dico[planet][day] = possibility
+        return self.dico
+
+    def update_dico(self, actions, old_state, current_info):
+    #update dico with the possible actions in days
+        #action = (planet, dist)
+        planet = current_info[1]
+        day = current_info[0]
+        for a in actions:
+            if a[0] == 'refuel':
+                new_state = { "fuel" : 6, "danger" : old_state["danger"] + self.danger(planet, day) }
+                if day + 1 in self.dico[planet].keys():
+                    self.dico[planet][day + 1].append( new_state ) 
+                else:
+                    self.dico[planet][day + 1] =  [new_state]
+            else:
+                if check_fuel(a, old_state) and self.check_countdown(a, day):#check if action is possible and if it will end in time
+                    new_state = { 'fuel' : old_state['fuel'] - a[1], "danger" : old_state["danger"] + self.danger(a[0], day)}
+                    if day + a[1] in self.dico[a[0]].keys():#virrable
+                        self.dico[a[0]][day + a[1]].append( new_state ) 
+                    else : 
+                        self.dico[a[0]][day + a[1]] = [new_state]
+
+    def danger(self, planet, day):
+        #check if a body hunter is on planet on day
+        for danger in self.empire_data["bounty_hunters"]:
+            if danger['planet'] == planet and danger['day'] == day:
+                return 1
+        return 0
+
+    def check_countdown(self, action, day):
+        #check if an action will end before final countdown
+        if day + action[1] > self.empire_data["countdown"]:
+            return False
+        else:
+            return True
+
+    def get_min_rencontre(self):
+        endor = self.dico['Endor']
+        print(endor)
+        min_r = 42000#pas ouf
+        for day in endor:
+            if endor[day]['danger'] < min_r:
+                min_r = endor[day]['danger']
+        return min_r
--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,5 @@
+import json
+
 def proba_of_wining (k):
     res = 0
     if k < 1000:
@@ -12,3 +14,40 @@ def check_fuel(action, old_state):
         return True
     else:
         return False
+
+def read_file(path):
+    with open(path) as json_file:
+        data = json.load(json_file)
+    return data
+
+def safest(list_p):
+    res = list_p[0]
+    for p in list_p:
+        if res['danger'] > p['danger']:
+            res = p
+    return res
+
+def build_pareto(list_p):
+    res = [list_p[0]]
+    for p in list_p:
+        if on_pareto(res, p):
+            res =update_pareto(res, p)
+    return res
+
+def on_pareto(list_p, p):
+    #return true if p is on pareto front of list_p
+    for i in list_p :
+        if i['danger'] <= p['danger'] and i['fuel'] >= p['fuel']:
+            return False
+    return True
+
+def update_pareto(list_p, possibility):
+    res = [possibility]
+    for p in list_p:
+        if p['danger'] < possibility['danger'] or p['fuel'] > possibility['fuel']:
+            res.append(p)
+    return res
+
+
+l = [{'danger' : 4, 'fuel' : 1}, {'danger' : 1, 'fuel' : 2}, {'danger' : 7, 'fuel' : 4}, {'danger' : 3, 'fuel' : 5}, {'danger' : 5, 'fuel' : 7} ]
+print(build_pareto(l))
