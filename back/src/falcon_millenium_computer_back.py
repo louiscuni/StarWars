84c69a3ebc485806f2dbb15b2303249780c3efcd
LouiCUNI <louicuni@gmail.com>
2021-02-26 13:35:18 +0100

    full refacto and add frontend

--- back/src/falcon_millenium_computer_back.py
+++ back/src/falcon_millenium_computer_back.py
@@ -17,19 +17,23 @@ class falco_M_computer:
         self.requetor = universe_requetor(db_path / vaisseau_data['routes_db'])
         self.dico = {}
 
+
     def init_dico(self):
         #initialize dico
         planets = self.requetor.get_all_planets()
         self.dico = tab_to_dico(planets)
         paths = self.shortest_paths(planets)
+        
         for p in planets:
             self.dico[p]['end_day'] = self.empire_data['countdown'] - paths[p] 
+        
         self.dico[self.vaisseau_data['departure']][0] = [{"fuel": self.vaisseau_data['autonomy'], "danger": 0, 'last_planet': [(self.vaisseau_data['departure'], 0)] }]    
+
 
     def build_dico(self):
         #build dico that represent all possible actions in the universe (the database)
         self.init_dico()
-        print(self.dico)
+
         for day in range(self.empire_data['countdown'] + 1):#iteration on days
             for planet in self.dico.keys():#iteration on planet
                 if day <= self.dico[planet]['end_day']:#check if planet is still useable
@@ -38,6 +42,7 @@ class falco_M_computer:
                         if day in self.dico[planet].keys():#check if it exist any possibility to be on this planet on this day
                             possibilities = build_pareto(self.dico[planet][day])
                             self.dico[planet][day] = possibilities
+
                             for p in possibilities:#iteration on possibilities
                                 self.update_dico( actions, p, [day, planet] )
                     else:
@@ -45,6 +50,7 @@ class falco_M_computer:
                             possibility = safest(self.dico[planet][day])
                             self.dico[planet][day] = possibility
         return self.dico
+
 
     def update_dico(self, actions, old_state, current_info):
     #update dico with the possible actions in days
@@ -81,8 +87,6 @@ class falco_M_computer:
 
         return nx.single_source_dijkstra_path_length(G, self.vaisseau_data['arrival'])
 
-    
-
 
     def danger(self, planet, day):
         #check if a body hunter is on planet on day
@@ -91,12 +95,14 @@ class falco_M_computer:
                 return 1
         return 0
 
+
     def check_countdown(self, action, day):
         #check if an action will end before final countdown
         if day + action[1] > self.empire_data["countdown"]:
             return False
         else:
             return True
+
 
     def get_min_rencontre(self):
         endor = self.dico['Endor']
@@ -106,6 +112,6 @@ class falco_M_computer:
             if day != 'end_day':
                 if endor[day]['danger'] < min_r:
                     min_r = endor[day]['danger']
-                if endor[day]['danger'] == 0:
-                    print(day, endor[day])
+                # if endor[day]['danger'] == 0:
+                #     print(day, endor[day])
         return min_r
---
+++ back/src/true_back.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify, render_template, request
+import json
+import ast
+from pathlib import Path
+import R2D2_CLI as r2d2
+
+
+def get_path():
+    p = Path.cwd()
+    #app.logger.info(p)
+    p = p.parent.parent / 'front' / 'templates' 
+    return str(p)
+
+path_template = get_path()
+
+app = Flask(__name__, static_url_path='', template_folder=path_template)
+
+@app.route('/machine_a_laver')
+def moteur():
+    data = request.args.get('data', type=str)
+    data = ast.literal_eval(data)
+    res = r2d2.main(data)
+    return jsonify(result=res)
+    #return json.dumps(data['bounty_hunters'])
+
+@app.route('/')
+def index():
+    #url = url_for('static', filename='s.js')
+    return render_template('C3PO_front.html')
+    #return json.dumps(plop())
+
+if __name__ == '__main__':
+    #b = single_bridge('b')#don't know if i can do that
+    app.run(host='127.0.0.1', port=5001, debug=True)
